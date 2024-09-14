e045a7b555328755db2c910f0965cc6a5034b2eb
Owen D'Aprile <git@owen.sh>
2024-09-13 21:12:01 -0400

    Add systemd service for 1Password
    Since 1Password exits every time rpm-ostree stages a deployment, create
    a user systemd service that automatically restarts it whenever it exits.

--- home.nix
+++ home.nix
@@ -492,6 +492,26 @@
     };
   };
 
+  systemd.user.services = {
+    "1password" = {
+      Unit = {
+        Description = "1Password password manager";
+        After = "graphical-session.target";
+        PartOf = "graphical-session.target";
+      };
+      Service = {
+        Type = "exec";
+        ExitType = "cgroup";
+        ExecStart = "/opt/1Password/1password --silent";
+        Restart = "always";
+        Slice = "app.slice";
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+  };
+
   programs.home-manager.enable = true;
   news.display = "silent";
   home.stateVersion = "24.05";
