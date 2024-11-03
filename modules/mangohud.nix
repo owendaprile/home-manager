b3e229a9495cc66975bfde8a246b17869e8d2a54
Owen D'Aprile <git@owen.sh>
2024-11-03 11:41:10 -0500

    mangohud: Re-enable options that work on AMD

--- modules/mangohud.nix
+++ modules/mangohud.nix
@@ -8,19 +8,19 @@
     settings = {
       cpu_stats = true;
       cpu_temp = true;
-      cpu_power = false;
+      cpu_power = true;
       cpu_mhz = true;
       cpu_load_change = true;
       cpu_load_color = "FFFFFF,FFFF00,FF0000";
 
-      gpu_stats = false;
+      gpu_stats = true;
       gpu_temp = true;
       gpu_core_clock = true;
       gpu_mem_clock = true;
       gpu_power = true;
       gpu_load_change = true;
       gpu_load_color = "FFFFFF,FFFF00,FF0000";
-      vram = false;
+      vram = true;
 
       fps = true;
       frame_timing = 10;
